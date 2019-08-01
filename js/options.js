0ca1f58d22a0a6c265126e7fb62a56068e8d0f3c
Evan Cooper <evan.cooper@shoebox.md>
2019-08-01 11:52:08 -0400

    Better options menu

--- js/options.js
+++ js/options.js
@@ -1,21 +1,77 @@
-let status = document.getElementById("status")
-let userBlacklist = document.getElementById('blacklist')
+let blacklist = document.getElementById('blacklist')
+let input = document.getElementById('newSubreddit')
 
-function save() {
-    status.textContent = "..."
-    chrome.storage.sync.set({
-        'blacklist': userBlacklist.value
-    }, function() {
-        status.textContent = "Saved."
+function addItem() {
+    let subreddit = input.value
+
+    fetch(function(result) {
+        var subreddits = result
+        if (subreddits == undefined) {
+            subreddits = []
+        }
+
+        subreddits.push(subreddit)
+        setList(subreddits)
+
+        save(subreddits, function() {
+            input.value = ""
+        })
+    })
+}
+
+function deleteItem(click) {
+    let target = click.currentTarget
+    let id = target.id
+    let index = id.split('-').pop()
+
+    fetch(function(result) {
+        result.splice(index, 1)
+        setList(result)
+        save(result, function() {
+            restore()
+        })
     })
 }
 
 function restore() {
-    chrome.storage.sync.get('blacklist', function(result) {
-        console.log(result)
-        userBlacklist.value = result.blacklist
+    fetch(function(result) {
+        if (result == undefined) return
+        setList(result)
+    })
+}
+
+function setList(list) {
+    blacklist.innerHTML = listHTML(list)
+    let buttons = document.getElementsByClassName('listButton')
+    Array.from(buttons).forEach(function(button) {
+        button.addEventListener('click', deleteItem)
+    })
+}
+
+function listHTML(list) {
+    var html = ""
+    list.forEach(element => {
+        let index = list.indexOf(element)
+        let button = "<button class=\"listButton\" id=button-" + index + ">Delete</button>"
+        html += "<li class=\"listItem\" id=li-" + index + ">" + element + button + "</li>"
+    });
+    return html
+}
+
+function save(value, handler) {
+    chrome.storage.sync.set({
+        blacklist: value
+    }, function() {
+        if (handler == undefined) return
+        handler()
+    })
+}
+
+function fetch(callback) {
+    chrome.storage.sync.get(['blacklist'], function(result) {
+        callback(result.blacklist)
     })
 }
 
 document.addEventListener('DOMContentLoaded', restore)
-document.getElementById('save').addEventListener('click', save)
+document.getElementById('addButton').addEventListener('click', addItem)
